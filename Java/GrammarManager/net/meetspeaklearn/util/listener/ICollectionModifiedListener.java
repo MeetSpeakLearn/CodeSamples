2039e3b07d1623dc1f660e5636b05a425fb56945
MeetSpeakLearn <149481087+MeetSpeakLearn@users.noreply.github.com>
2023-10-31 12:17:49 +0100

    Add files via upload

---
+++ Java/GrammarManager/net/meetspeaklearn/util/listener/ICollectionModifiedListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.meetspeaklearn.util.listener;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author steve
+ */
+public interface ICollectionModifiedListener {
+    public void collectionModified(Collection stuff);
+}
